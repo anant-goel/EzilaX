ef57b8be8b80f82e346ec323e4c6cc09a6bad84d
anant-goel <85389190+anant-goel@users.noreply.github.com>
2022-02-12 18:46:11 +0530

    Update sample_config.py

--- EzilaXBot/sample_config.py
+++ EzilaXBot/sample_config.py
@@ -14,27 +14,27 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 4652853  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1548967589  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Darkridersslk" 
-    SUPPORT_CHAT = "SDBOTz"  # Your own group for support, do not add the @
+    API_ID = 17372560  # integer value, dont use ""
+    API_HASH = "e4d5f831cc33652445762eaf8a83aa62"
+    TOKEN = "5271886587:AAHL6G8-k0w7oC65YiNLf6R95oDfj4FiuKo"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 873948892  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Anant_Goel 
+    SUPPORT_CHAT = "PcGamesAllForFree"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001130413114
+        -1001737747308
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001130413114
+        -793784681
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
     LOAD = []
-    NO_LOAD = ["rss", "cleaner", "connection", "math"]
+    NO_LOAD = []
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "3H3pmefnq5hLxdQ5Zv9shJSW6DVrItDGcnasYKIPYUfbP5RdSBHQkMfCdjLoQuXM"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "SpamWatchSupport"
 
     # OPTIONAL
@@ -55,16 +55,16 @@ class Config(object):
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    BAN_STICKER = "CAACAgQAAx0CU_rCTAABAczQXyBOv1TsVK4EfwnkCUT1H0GCkPQAAtwAAwEgTQaYsMtAltpEwhoE"  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "7O0TUX4ITJQUVXWC"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "IZW26DEW90AI"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
-        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+        ""  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    AI_API_KEY = "9a43a0f251cd1f5366dba5502df57843e926acfd653b4e3f53a4ba49fec6b3732b25fb389ec1aa1d702c9238d10c5e2ce3506758dc3fce5f278d333775787eca"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
